e6920a2011ea554c576abf399a81d9e2bc918cb6
Hernan Badino <hbadino@rec.ri.cmu.edu>
2012-09-10 17:22:48 -0400

    Adding copyright and license description to new files

--- clockTreeItemModel.cpp
+++ clockTreeItemModel.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2012 Hernan Badino <hernan.badino@gmail.com>
+ *
+ * This file is part of QCV
+ *
+ * QCV is under the terms of the GNU General Public License, version 2.
+ * See the GNU General Public License, version 2 for details.
+ * QCV is distributed "AS IS" without ANY WARRANTY, without even the
+ * implied warranty of merchantability or fitness for a particular
+ * purpose. 
+ *
+ * In no event shall the authors or contributors be liable
+ * for any direct, indirect, incidental, special, exemplary, or
+ * consequential damages arising in any way out of the use of this
+ * software.
+ *
+ * By downloading, copying, installing or using the software you agree
+ * to this license. Do not download, install, copy or use the
+ * software, if you do not agree to this license.
+ */
+
 /*@@@**************************************************************************
  * \file  clockTreeItemModel
  * \date  Tue Apr 14 17:04:52 GMT 2009

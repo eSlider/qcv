b6217b0e1b3a72ce52bc708880e56e327f59439c
Hernan Badino <hbadino@rec.ri.cmu.edu>
2014-07-07 16:07:30 -0400

    Updated copyright info
    --HG--
    branch : notanumber.devel

--- modules/operators/featureStereoOp.cpp
+++ modules/operators/featureStereoOp.cpp
@@ -1,16 +1,23 @@
-/**
-*******************************************************************************
-*
-* @file featureStereoOp.cpp
-*
-* \class CFeatureStereoOp
-* \date  Tue Oct 13, 2009
-* \author Hernan Badino (hernan.badino@gmail.com)
-*
-* \brief Framework operator class for computing ego-motion from 3D point
-*        correspondences between current and previous frame.
-*
-*******************************************************************************/
+/*
+ * Copyright (C) 2012 Hernan Badino <hernan.badino@gmail.com>
+ *
+ * This file is part of QCV
+ *
+ * QCV is under the terms of the GNU Lesser General Public License
+ * version 2.1. See the GNU LGPL version 2.1 for details.
+ * QCV is distributed "AS IS" without ANY WARRANTY, without even the
+ * implied warranty of merchantability or fitness for a particular
+ * purpose. 
+ *
+ * In no event shall the authors or contributors be liable
+ * for any direct, indirect, incidental, special, exemplary, or
+ * consequential damages arising in any way out of the use of this
+ * software.
+ *
+ * By downloading, copying, installing or using the software you agree
+ * to this license. Do not download, install, copy or use the
+ * software, if you do not agree to this license.
+ */
 
 /* INCLUDES */
 #include <limits>
